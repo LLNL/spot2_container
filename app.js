c7284bc987afa26f4085731ae145eb9aa2edb641
Pascal Aschwanden <aschwanden1@llnl.gov>
2020-12-22 16:17:43 -0800

    Update app.js to the newest api for getmemory

--- app.js
+++ app.js
@@ -14,11 +14,39 @@ app.post('/getdata',(req, res) =>{
 
     const command = "/opt/conda/bin/python3 /usr/gapps/spot/backend.py --config /usr/gapps/spot/backend_config.yaml getData /data/" + 
                      req.body.dataSetKey + 
-                     " '" + JSON.stringify(req.body.cachedRunCtimes) + "'"  
+                     " '" + JSON.stringify(req.body.cachedRunCtimes) + "'"
+
+    console.log( command );
+
     exec(command, {maxBuffer:1024*1024*1024}, (err, stdout, stderr) => {
             res.send(stdout.toString())
         })
 })
+
+
+app.post('/getmemory',(req, res) =>{
+
+    console.log('app.js /getmemory  xxsdfg');
+
+    var filepath = "lul_sept_28_timeseries/200924-16454258362.cali";
+
+    //  /usr/gapps/spot/venv_python/bin/python3 /usr/gapps/spot/dev/spot.py memory /usr/gapps/spot/datasets/lulesh_gen/100/1.cali
+    const command = "/opt/conda/bin/python3 /usr/gapps/spot/backend.py --config /usr/gapps/spot/backend_config.yaml getData /data/" +
+        filepath;
+
+    var command2 = "ls -l";
+    var command3 = "/opt/conda/bin/python3 /usr/gapps/spot/backend.py --config /usr/gapps/spot/backend_config.yaml memory /data/lul_sept_28_timeseries";
+
+    console.log(command3);
+    console.log(req.body.filepath);
+
+    exec(command3, {maxBuffer:1024*1024*1024}, (err, stdout, stderr) => {
+
+            console.log("memory resposne")
+            res.send(stdout.toString())
+        })
+})
+
 
 app.post('/spotJupyter',(req, res) =>{
     const command = `/opt/conda/bin/python3 /usr/gapps/spot/backend.py --config /usr/gapps/spot/backend_config.yaml --container jupyter '${req.body.filepath}'`
