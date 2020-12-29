8fb543963a68aae43191c023e8b65ac556dcf9ef
Matthew LeGendre <legendre1@llnl.gov>
2020-12-29 11:52:29 -0800

    Use tokens to access jupyter in container mode

--- runspot.sh
+++ runspot.sh
@@ -4,8 +4,14 @@ if [[ $UID == 0 ]] ; then
     exec sudo -u spot `realpath $0` $*
 fi
 
+jupytertoken=`tr -dc A-Za-z0-9 < /dev/urandom | head -c 16`
+
+rm -f /tmp/jupytertoken
+umask 0077
+echo $jupytertoken > /tmp/jupytertoken
+
 node /usr/gapps/spot/app.js &
-/opt/conda/bin/jupyter notebook --notebook-dir=/notebooks --NotebookApp.token="" --NotebookApp.password="" --no-browser &
+/opt/conda/bin/jupyter notebook --notebook-dir=/notebooks --NotebookApp.token=$jupytertoken --NotebookApp.password="" --no-browser &
 
 wait
 
